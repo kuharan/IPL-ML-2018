5b306ed35d38d7ce5ee27e99c6a5122def1e1f40
vishal-kr-yadav <vkyadav9748@gmail.com>
2018-04-23 11:38:56 +0530

    if else added in predictions

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from Prediction import pred
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,flash
 
-app: Flask = Flask(__name__)
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -13,7 +15,8 @@ def second():
         toss_winner = request.form['tossWinner']
         toss_decision = request.form['selector1']
         print(home_team, away_team, city, toss_winner, toss_decision)
-        pred(home_team, away_team, city, toss_winner, toss_decision)
+        winner_team=pred(home_team, away_team, city, toss_winner, toss_decision)
+        flash("winning team=="+winner_team)
     return render_template('index.html')
 
 
